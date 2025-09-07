c7fec43563cb74bf1078ffd6329245501d521f0e
Chaitanya Sagar Kuracha <60644758+cskuracha@users.noreply.github.com>
2025-09-07 15:00:02 +0530

    Regular auto-commit at 15:00:02 on 07/09/2025

---
+++ AgenticCalc/main.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+def main():
+    print("Hello from agenticcalc!")
+
+
+if __name__ == "__main__":
+    main()
